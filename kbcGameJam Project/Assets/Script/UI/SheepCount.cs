11c33941db1532abe34f2f7faef9dfbd20542ddf
Satoshi Yoshinaga <kbc18b31@stu.kawahara.ac.jp>
2019-10-08 17:54:12 +0900

    timerとSheepの当たり判定(修正が必要かも)とカウントを制作 当たり判定は距離で判定しなければいけないかも

---
+++ kbcGameJam Project/Assets/Script/UI/SheepCount.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SheepCount : MonoBehaviour
+{
+    aboutmove about;
+    int count;
+    public Text numText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SheepnumCount();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Sheep"))
+        {
+            Debug.Log("羊じゃい！");
+            count++;
+            SheepnumCount();
+        }
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("犬じゃい!");
+        }
+    }
+    void SheepnumCount()
+    {
+        numText.text = string.Format("捕まえた羊の数:{0}", count);
+    }
+}
