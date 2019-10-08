3d43efb6352d93ec699382cc5021da988999d174
komura athushi <kbc18b09@kawahara.ac.jp>
2019-10-08 13:02:36 +0900

    a

---
+++ kbcGameJam Project/Assets/Script/GameCameraController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCameraController : MonoBehaviour
+{
+    Transform m_target;
+    Transform m_transform;
+    Camera m_cam;
+    float m_degreeH = 180.0f;
+    float m_degreeV = 50.0f;
+    Vector3 m_front = Vector3.zero;
+    Vector3 m_right = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_target = GameObject.Find("Player").GetComponent<Transform>();
+        m_transform = this.GetComponent<Transform>();
+     
+    }
+    public Vector3 GetFront()
+    {
+        return m_front;
+    }
+    public Vector3 GetRight()
+    {
+        return m_right;
+    }
+
+    [System.Obsolete]
+    void TPSCamera()
+    {
+        const float radius = 8.0f;
+        const float targetHeight = 1.5f;
+        const float targetFront = 0.3f;
+
+        float degreeH = Input.GetAxis("Horizontal2") * 2.3f;
+        float degreeV = 0.0f; //= Input.GetAxis("Vertical2") * 2.3f;
+
+        m_degreeH += degreeH;
+        m_degreeV += degreeV;
+
+        if (m_degreeV >= 40.0f)
+        {
+            m_degreeV = 40.0f;
+        }
+        else if (m_degreeV <= -15.0f)
+        {
+            m_degreeV = -15.0f;
+        }
+        float angleH = Mathf.PI / 180.0f * m_degreeH;
+        float angleV = Mathf.PI / 180.0f * m_degreeV;
+        //float angleH = m_degreeH;
+        //float angleV = m_degreeV;
+
+
+        Vector3 targetCamPos = Vector3.zero;
+        targetCamPos.y += targetHeight;
+        targetCamPos += m_target.position;
+
+        Quaternion qRot = Quaternion.identity;
+        Vector3 axisY = new Vector3(0.0f, 1.0f, 0.0f);
+        qRot.SetAxisAngle(axisY, angleH);
+
+        Vector3 toPos = new Vector3(0.0f, 0.0f, 1.0f);
+        toPos = qRot * toPos;
+
+        Vector3 rotAxis = Vector3.zero;
+        rotAxis = Vector3.Cross(toPos, axisY);
+        rotAxis = rotAxis.normalized;
+
+        qRot.SetAxisAngle(rotAxis, angleV);
+        toPos = qRot * toPos;
+        toPos *= radius;
+        Vector3 position = targetCamPos + toPos;
+
+        transform.position = position;
+        toPos.y = 0.0f;
+        targetCamPos -= toPos * targetFront;
+        transform.LookAt(targetCamPos);
+
+        m_front = targetCamPos - position;
+        m_front = m_front.normalized;
+        m_right = m_front;
+        Quaternion rot = Quaternion.identity;
+        rot.SetAxisAngle(axisY, -90.0f * Mathf.PI / 180.0f);
+        m_right = rot * m_right;
+    }
+    // Update is called once per frame
+    [System.Obsolete]
+    void Update()
+    {
+        TPSCamera();
+    }
+}
---
+++ kbcGameJam Project/Assets/Script/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   
+    Transform m_transform;
+    Rigidbody m_rigidbody;
+    GameCameraController m_gameCameraController;
+    bool m_isBark = false;
+    float m_timer = 0.0f;
+
+    //吠えたかどうか
+    bool GetisBark()
+    {
+        return m_isBark;
+    }
+    //犬の吠えたベクトルを取得
+    Vector3 GetVector()
+    {
+        const float LONG = 10.0f;
+        return LONG * transform.forward;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_transform = this.GetComponent<Transform>();
+        m_rigidbody = this.GetComponent<Rigidbody>();
+        m_gameCameraController = GameObject.Find("GameCamera").GetComponent<GameCameraController>();
+    }
+
+    void LookDownCamera()
+    {
+        const float SPEED = 10.15f;
+
+        
+        Vector3 moveSpeed = new Vector3(0.0f,0.0f,0.0f);
+        moveSpeed.x = Input.GetAxis("Horizontal") * SPEED;
+        moveSpeed.z = Input.GetAxis("Vertical") * SPEED;
+        //m_transform.position += moveSpeed;
+        //rigidbody.velocity = moveSpeed;
+        m_rigidbody.velocity = moveSpeed;
+        Quaternion rotation = new Quaternion();
+        rotation.SetLookRotation(moveSpeed);
+        m_transform.rotation = rotation;
+    }
+    void MoveRot()
+    {
+        const float MOVESPEED = 13.0f;
+
+        Vector3 front = m_gameCameraController.GetFront();
+        Vector3 right = m_gameCameraController.GetRight();
+
+        right *= Input.GetAxis("Horizontal") * -MOVESPEED;
+        front *= Input.GetAxis("Vertical") * MOVESPEED;
+
+        Vector3 moveSpeed;
+        moveSpeed = front + right;
+        moveSpeed.y = 0.0f;
+        m_rigidbody.velocity = moveSpeed;
+        if(moveSpeed.magnitude <= 0.01f)
+        {
+            return;
+        }
+        Quaternion rot = new Quaternion();
+        rot.SetLookRotation(moveSpeed);
+        transform.rotation = rot;
+    }
+    void Bark()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MoveRot();
+        
+    }
+}
