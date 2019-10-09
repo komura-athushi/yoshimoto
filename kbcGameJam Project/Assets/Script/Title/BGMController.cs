8d90d5436b3d5c6796ff3821ea6b9c16f553d989
komura athushi <kbc18b09@kawahara.ac.jp>
2019-10-09 10:23:10 +0900

    Scriptのファイル分割、BGMを永続で再生

---
+++ kbcGameJam Project/Assets/Script/Title/BGMController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour
+{
+    //シングルトン
+    private static GameObject m_instance = null;
+
+    public static GameObject Instance
+    {
+        get { return m_instance; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+        if (m_instance != null && m_instance != this.gameObject)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            m_instance = this.gameObject;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
