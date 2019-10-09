e52154e58e8cc842df7635ad2469bd20dac57f58
komura athushi <kbc18b09@kawahara.ac.jp>
2019-10-09 14:04:47 +0900

    a

--- kbcGameJam Project/Assets/Script/UI/SheepCount.cs
+++ kbcGameJam Project/Assets/Script/UI/SheepCount.cs
@@ -5,25 +5,42 @@ using UnityEngine.UI;
 
 public class SheepCount : MonoBehaviour
 {
-    aboutmove about;
-    int count;
+    public static int SHEEPCOUNT = 0;
+    GameObject[] m_sheepList;
     public Text numText;
-    public float kyori;
-    public GameObject PosSheep;
+    public float DISTANCE;
+    public Vector3 POSITION;
     // Start is called before the first frame update
     void Start()
     {
-        count = 0;
+        SHEEPCOUNT = 0;
         SheepnumCount();
+        m_sheepList = GameObject.FindGameObjectsWithTag("Sheep");
     }
 
     //Update is called once per frame
-    //void Update()
-    //{
-    //
-    //}
-
-    void OnTriggerEnter(Collider other)
+    void Update()
+    {
+        SheepnumCount();
+        CaptureSheep();
+    }
+    void CaptureSheep()
+    {
+        foreach (GameObject sheep in m_sheepList)
+        {
+            SheepController sheepController = sheep.GetComponent<SheepController>();
+            if(!sheepController.GetisCapture())
+            {
+                Vector3 distance = sheep.transform.position - POSITION;
+                if(distance.sqrMagnitude <= DISTANCE * DISTANCE)
+                {
+                    SHEEPCOUNT += 1;
+                    sheepController.SetisCapture();
+                }
+            }
+        }
+    }
+    /*void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Sheep"))
         {
@@ -35,9 +52,9 @@ public class SheepCount : MonoBehaviour
         {
             Debug.Log("犬じゃい!");
         }
-    }
+    }*/
     void SheepnumCount()
     {
-        numText.text = string.Format("捕まえた羊の数:{0}", count);
+        numText.text = string.Format("捕まえた羊の数:{0}", SHEEPCOUNT);
     }
 }
